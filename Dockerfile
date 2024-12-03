132f46683e16772d73248a2a008e538d514de9de
Krystian Chachuła <krystian@krystianch.com>
2024-12-03 15:11:00 +0100

    Add sqlite support

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM alpine:3.20.3
 
-RUN apk add python3 py3-pip
+RUN apk add python3 py3-pip sqlite
 
 COPY requirements.txt /
 RUN pip install --break-system-packages -r requirements.txt && rm requirements.txt
--- llmproxy/db.py
+++ llmproxy/db.py
@@ -1,14 +1,22 @@
 import asyncio
 import datetime
 import hashlib
+import sqlite3
 
+import aiosqlite
 import motor.motor_asyncio
 import pymongo.server_api
 
 async def get_db(req):
     if "db" not in req:
         uri = req.app["config"]["db"]["uri"]
-        req["db"] = await Database.create(uri, req.app.logger)
+
+        if uri.startswith("mongodb://"):
+            req["db"] = await MongoDatabase.create(uri, req.app.logger)
+        elif uri.startswith("sqlite://"):
+            req["db"] = await SqliteDatabase.create(uri, req.app.logger)
+        else:
+            raise ValueError("Unrecognized URI scheme: \"%s\"" % uri)
 
     return req["db"]
 
@@ -17,7 +25,7 @@ class DatabaseError(Exception):
     pass
 
 
-class Database:
+class MongoDatabase:
     @classmethod
     async def create(cls, uri, logger):
         self = cls()
@@ -67,3 +75,59 @@ class Database:
             })
         except pymongo.errors.PyMongoError as e:
             raise DatabaseError(e) from e
+
+
+class SqliteDatabase:
+    @classmethod
+    async def create(cls, uri, logger):
+        path = uri.removeprefix("sqlite://")
+        assert path != uri
+
+        self = cls()
+        self.logger = logger
+        self.db = await aiosqlite.connect(path)
+        self.logger.debug("Connected to database")
+
+        return self
+
+    async def close(self):
+        await self.db.close()
+        self.logger.debug("Closed database connection")
+
+    async def get_user(self, api_key):
+        digest = hashlib.sha256(api_key.encode()).hexdigest()
+
+        try:
+            cur = await self.db.execute("""
+                SELECT id FROM api_key WHERE type = 'LLM' AND secret = ?
+                    AND (expires > datetime() OR expires IS NULL)
+                """, (digest,))
+        except sqlite3.DatabaseError as e:
+            raise DatabaseError(e) from e
+
+        row = await cur.fetchone()
+        if row is not None:
+            row = row[0]
+
+        await cur.close()
+
+        return row
+
+    async def put_event(self, user, time, model, device, prompt_n, completion_n, request_id):
+        product_prompt = "%s/%s/prompt" % (model, device)
+        product_completion = "%s/%s/completion" % (model, device)
+
+        try:
+            await self.db.execute("""
+                INSERT INTO event_oneoff (created, api_key, product, quantity, rid)
+                VALUES (?, ?, ?, ?, ?)
+                """, (time, user, product_prompt, prompt_n, str(request_id)))
+
+            await self.db.execute("""
+                INSERT INTO event_oneoff (created, api_key, product, quantity, rid)
+                VALUES (?, ?, ?, ?, ?)
+                """, (time, user, product_completion, prompt_n, str(request_id)))
+        except sqlite3.DatabaseError as e:
+            raise DatabaseError(e) from e
+
+        await self.db.commit()
